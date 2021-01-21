9addbba2e3becc1525c192c41d82c32b520a5584
Tony Guan <t9guan@ucsd.edu>
2021-01-21 10:46:04 -0800

    add interactivity

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,28 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Hello, world.");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e){
+	console.log("Project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "green");
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $(this).fadeOut();
+	}
+
+	
 }
